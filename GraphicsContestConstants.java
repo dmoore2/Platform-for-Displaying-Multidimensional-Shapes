e89cd33d3346eab020bb62698f7f1f4e9bf24550
Dylan Moore <dmoore2@stanford.edu>
2014-01-05 14:35:16 -0800

    Create GraphicsContestConstants.java

---
+++ GraphicsContestConstants.java
@@ -0,0 +1,24 @@
+import java.awt.Color;
+ 
+ 
+public interface GraphicsContestConstants {
+ 
+    //max amount of characters that can be put into the name text box4
+    public static final Color[] COLORS_ARRAY= new Color[]{Color.green, Color.blue, Color.cyan, Color.magenta, Color.pink, Color.white, Color.yellow};
+    public static final String[] COLORS_NAME_ARRAY = new String[]{"Green", "Blue", "Cyan", "Magenta", "Pink", "White", "Yellow", "Random"};
+    public static final int INITIAL_ROTATION_SPEED = 15;
+    public static final double DEFAULT_CIRCLE_SCALE = 25;
+    public static final double CIRCLE_SCALAR = 15;
+    public static final double SPEED_BASE = 2;
+    public static final String[] ARRAY_OF_SHAPES = {"Square", "Cube", "Tesseract", "Penteract", "Hexeract", "Hepteract", "Icosahedron"};
+     
+    public static final int INITIAL_ZOOM = 70;
+     
+    public static final double SPEED_SCALAR = 0.000034;
+     
+    public static final double SCALAR = 350;
+     
+    public static final String filename = "testFile.txt";
+ 
+}
+ 
